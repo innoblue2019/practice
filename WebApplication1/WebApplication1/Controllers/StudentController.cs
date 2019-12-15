97e215abd4b862f33cbe8aef91436d2b14fa9edf
YaoteHuang <y69627@gmail.com>
2019-12-15 16:51:19 +0800

    init2

---
+++ WebApplication1/WebApplication1/Controllers/StudentController.cs
@@ -0,0 +1,33 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    
+
+    public class StudentController : Controller
+    {
+        string strConnection = "";
+
+
+        // GET: Student
+        public ActionResult Index()
+        {
+            //Query Model
+            List<Student> results = null;
+            using (SqlConnection conn = new SqlConnection(strConnection))
+            {
+                string strSql = "Select * from Student";
+                results = conn.Query<Student>(strSql).ToList();
+            }
+
+            return View(results);
+        }
+    }
+}
